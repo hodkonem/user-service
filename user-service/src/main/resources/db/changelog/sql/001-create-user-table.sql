cd845815ba1e80e43a40830d8b795bc234fa89f5
Mikhail Latypov <hodkonem1984@gmail.com>
2026-01-18 00:38:16 +0300

    feat: add postgres compose, env config and tidy liquibase changeset

--- user-service/src/main/resources/db/changelog/sql/001-create-user-table.sql
+++ user-service/src/main/resources/db/changelog/sql/001-create-user-table.sql
@@ -1,6 +1,6 @@
 --liquibase formatted sql
 
---changeset yourname:001-create-user-table
+--changeset hodkonem:001-create-user-table
 CREATE TABLE users (
                        id BIGSERIAL PRIMARY KEY,
                        name VARCHAR(100) NOT NULL,
